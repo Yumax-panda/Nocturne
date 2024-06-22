a44061f035e6a3b16cdf468af4b390d95e2b9150
Yumax-panda <yuma0wall@gmail.com>
2024-06-23 03:35:21 +0900

    エラーが出ているので一旦削除

--- src/router/router.ts
+++ src/router/router.ts
@@ -2,5 +2,3 @@ import type { Params, Result, Router } from "../router";
 import { SupportedMethod } from "../types";
 
 const MISSING = Symbol("missing sentinel");
-
-export class LinearRouter<T> implements Router<T> {}
