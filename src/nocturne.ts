e90ffce5f455c01329570ce40b2aa8cd0f955da0
Yumax-panda <yuma0wall@gmail.com>
2024-06-27 10:21:20 +0900

    insertNodeを実装

--- src/nocturne.ts
+++ src/nocturne.ts
@@ -5,15 +5,23 @@ export const RouteNotFound = "nocturne_route_not_found";
 
 export class Nocturne<E extends Env = EmptyEnv> {
 	env: E;
+	maxParam: number;
 	router: Router<E>;
 	routers: Record<string, Router<E>>;
 	middlewares: Middleware<E>[];
 }
 
 export class Route {
-	constructor(
-		public method: SupportedMethod,
-		public path: string,
-		public name: string,
-	) {}
+	method: SupportedMethod;
+	path: string;
+	name: string;
+	constructor({
+		method,
+		path,
+		name,
+	}: { method: SupportedMethod; path: string; name: string }) {
+		this.method = method;
+		this.path = path;
+		this.name = name;
+	}
 }
--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,5 @@
 import type { Nocturne } from "./nocturne";
-import { type Route, RouteNotFound } from "./nocturne";
+import { Route, RouteNotFound } from "./nocturne";
 import type { Env, Handler, SupportedMethod } from "./types";
 
 enum AllowHeader {
@@ -14,6 +14,12 @@ enum AllowHeader {
 	PUT = "PUT",
 	TRACE = "TRACE",
 	REPORT = "REPORT",
+}
+
+enum Kind {
+	Static = 0,
+	Param = 1,
+	Any = 2,
 }
 
 const PARAM_LABEL = ":";
@@ -132,7 +138,7 @@ export class Router<E extends Env> {
 	 * @returns 生成したURI
 	 */
 	reverse(name: string, ...params: { toString: () => string }[]): string {
-		const route = this._routes[name];
+		const route = this.routes.find((r) => r.name === name);
 
 		if (!route) {
 			return "";
@@ -158,27 +164,270 @@ export class Router<E extends Env> {
 
 		return path;
 	}
+
+	add(
+		method: SupportedMethod,
+		path: string,
+		name: string,
+		handler: Handler<E>,
+	): Route {
+		const normalized = normalizePathSlash(path);
+		this.insert(method, normalized, handler);
+
+		const route = new Route({ method, path: normalized, name });
+		const identifier = `${method}${normalized}`;
+		this._routes[identifier] = route;
+		return route;
+	}
+
+	private insertNode(
+		method: SupportedMethod,
+		path: string,
+		t: Kind,
+		rm: RouteMethod<E>,
+	): void {
+		const paramsLen = rm.paramNames.length;
+
+		if (this.n.maxParam < paramsLen) {
+			this.n.maxParam = paramsLen;
+		}
+
+		let currentNode = this.tree;
+		let search = path;
+
+		while (true) {
+			const searchLen = search.length;
+			const prefixLen = currentNode.prefix.length;
+			let lcpLen = 0;
+
+			let max = prefixLen;
+
+			if (searchLen < max) {
+				max = searchLen;
+			}
+
+			while (
+				lcpLen < searchLen &&
+				lcpLen < prefixLen &&
+				search[lcpLen] === currentNode.prefix[lcpLen]
+			) {
+				lcpLen++;
+			}
+
+			if (lcpLen === 0) {
+				currentNode.label = search[0];
+				currentNode.prefix = search;
+
+				if (rm.handler) {
+					currentNode.kind = t;
+					currentNode.addMethod(method, rm);
+					currentNode.paramsCount = rm.paramNames.length;
+					currentNode.originalPath = rm.pristinePath;
+				}
+				const isLeaf =
+					currentNode.staticChildren.length === 0 &&
+					!currentNode.paramChild &&
+					!currentNode.anyChild;
+
+				currentNode.isLeaf = isLeaf;
+			} else if (lcpLen < prefixLen) {
+				const newNode = new Node<E>({
+					t: currentNode.kind,
+					pre: currentNode.prefix.slice(lcpLen),
+					p: currentNode,
+					sc: currentNode.staticChildren,
+					originalPath: currentNode.originalPath,
+					methods: currentNode.methods,
+					paramsCount: currentNode.paramsCount,
+					paramChildren: currentNode.paramChild,
+					anyChildren: currentNode.anyChild,
+					notFound: currentNode.notFound,
+				});
+
+				for (const child of currentNode.staticChildren) {
+					child.parent = newNode;
+				}
+
+				if (currentNode.paramChild) {
+					currentNode.paramChild.parent = newNode;
+				}
+
+				if (currentNode.anyChild) {
+					currentNode.anyChild.parent = newNode;
+				}
+
+				currentNode.kind = Kind.Static;
+				currentNode.label = currentNode.prefix[0];
+				currentNode.prefix = currentNode.prefix.slice(0, lcpLen);
+				currentNode.staticChildren = [];
+				currentNode.originalPath = "";
+				currentNode.methods = new RouteMethods<E>({});
+				currentNode.paramsCount = 0;
+				currentNode.paramChild = undefined;
+				currentNode.anyChild = undefined;
+				currentNode.isLeaf = false;
+				currentNode.isHandler = false;
+				currentNode.notFound = undefined;
+
+				currentNode.addStaticChild(newNode);
+
+				if (lcpLen === searchLen) {
+					currentNode.kind = t;
+					if (rm.handler) {
+						currentNode.addMethod(method, rm);
+						currentNode.paramsCount = rm.paramNames.length;
+						currentNode.originalPath = rm.pristinePath;
+					}
+				} else {
+					const newNode = new Node<E>({
+						t: t,
+						pre: search.slice(lcpLen),
+						p: currentNode,
+						sc: [],
+						originalPath: "",
+						methods: new RouteMethods<E>({}),
+						paramsCount: 0,
+						paramChildren: undefined,
+						anyChildren: undefined,
+						notFound: undefined,
+					});
+
+					if (rm.handler) {
+						newNode.addMethod(method, rm);
+						newNode.paramsCount = rm.paramNames.length;
+						newNode.originalPath = rm.pristinePath;
+					}
+
+					currentNode.addStaticChild(newNode);
+				}
+				const isLeaf =
+					currentNode.staticChildren.length === 0 &&
+					!currentNode.paramChild &&
+					!currentNode.anyChild;
+				currentNode.isLeaf = isLeaf;
+			} else if (lcpLen < searchLen) {
+				search = search.slice(lcpLen);
+				const child = currentNode.findStaticChild(search[0]);
+
+				if (child) {
+					currentNode = child;
+					continue;
+				}
+
+				const newNode = new Node<E>({
+					t,
+					pre: search,
+					p: currentNode,
+					sc: [],
+					originalPath: rm.pristinePath,
+					methods: new RouteMethods<E>({}),
+					paramsCount: 0,
+					paramChildren: undefined,
+					anyChildren: undefined,
+					notFound: undefined,
+				});
+
+				if (rm.handler) {
+					newNode.addMethod(method, rm);
+					newNode.paramsCount = rm.paramNames.length;
+				}
+
+				if (t === Kind.Static) {
+					currentNode.addStaticChild(newNode);
+				} else if (t === Kind.Param) {
+					currentNode.paramChild = newNode;
+				} else {
+					currentNode.anyChild = newNode;
+				}
+
+				const isLeaf =
+					currentNode.staticChildren.length === 0 &&
+					!currentNode.paramChild &&
+					!currentNode.anyChild;
+
+				currentNode.isLeaf = isLeaf;
+			} else {
+				if (rm.handler) {
+					currentNode.addMethod(method, rm);
+					currentNode.paramsCount = rm.paramNames.length;
+					currentNode.originalPath = rm.pristinePath;
+				}
+			}
+			return;
+		}
+	}
+	// TODO
+	insert(method: SupportedMethod, path: string, handler: Handler<E>): void {
+		const normalized = normalizePathSlash(path);
+		const segments = normalized.split("/");
+		let current = this.tree;
+		let paramsCount = 0;
+
+		for (const segment of segments) {
+			if (segment === "") continue;
+
+			const node = current.findChildWithLabel(segment);
+
+			if (node) {
+				current = node;
+				continue;
+			}
+
+			const newNode = new Node<E>({
+				t: segment[0] === PARAM_LABEL ? 1 : 0,
+				pre: segment,
+				p: current,
+				sc: [],
+				originalPath: normalized,
+				methods: new RouteMethods<E>({}),
+				paramsCount,
+			});
+
+			if (segment[0] === PARAM_LABEL) {
+				current.paramChild = newNode;
+				paramsCount++;
+			} else if (segment[0] === ANY_LABEL) {
+				current.anyChild = newNode;
+			} else {
+				current.addStaticChild(newNode);
+			}
+
+			current = newNode;
+		}
+
+		const routeMethod = new RouteMethod<E>({
+			handler,
+			pristinePath: normalized,
+		});
+		current.addMethod(method, routeMethod);
+	}
 }
+
+const normalizePathSlash = (path: string): string => {
+	if (!path.startsWith("/")) return `/${path}`;
+
+	return path;
+};
 
 type Children<E extends Env> = Node<E>[];
 
 export class RouteMethod<E extends Env> {
-	handler: Handler<E>;
-	path: string;
-	names: string[];
+	handler?: Handler<E>;
+	pristinePath: string;
+	paramNames: string[];
 
 	constructor({
 		handler,
-		path = "",
-		names = [],
+		pristinePath = "",
+		paramNames = [],
 	}: {
-		handler: Handler<E>;
-		path?: string;
-		names?: string[];
+		handler?: Handler<E>;
+		pristinePath?: string;
+		paramNames?: string[];
 	}) {
 		this.handler = handler;
-		this.path = path;
-		this.names = names;
+		this.pristinePath = pristinePath;
+		this.paramNames = paramNames;
 	}
 }
 
@@ -188,17 +437,17 @@ export class Node<E extends Env> {
 	paramChild?: Node<E>;
 	anyChild?: Node<E>;
 	notFound?: RouteMethod<E>;
-	prefix?: string;
+	prefix: string;
 	originalPath: string;
 	staticChildren: Children<E>;
 	paramsCount: number;
 	label: string;
-	kind: number;
+	kind: Kind;
 	isLeaf: boolean;
 	isHandler: boolean;
 
 	constructor({
-		t = 0,
+		t = Kind.Static,
 		pre = "",
 		p = undefined,
 		sc = [],
@@ -209,7 +458,7 @@ export class Node<E extends Env> {
 		anyChildren = undefined,
 		notFound = undefined,
 	}: {
-		t?: number;
+		t?: Kind;
 		pre?: string;
 		p?: Node<E> | undefined;
 		sc?: Children<E>;
@@ -345,9 +594,3 @@ export class Node<E extends Env> {
 		}
 	}
 }
-
-const normalizePathSlash = (path: string): string => {
-	if (!path.startsWith("/")) return `/${path}`;
-
-	return path;
-};
